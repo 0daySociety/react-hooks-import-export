9075b224447e69c11535e20205d997f9821fdb76
--replace <david.thuku@studentmoringa.school.com>
2023-07-19 08:33:40 +0300

    Done with test

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,19 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import { trees,wildlife,elevation } from "./parks/RockyMountain";
+import MesaVerde from "./parks/MesaVerde";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  wildlife();
+  elevation()
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+  <div>
+     <h1>Colorado State Parks!</h1>
+     <MesaVerde />
+  </div>
+ 
+
+  );
 }
